ab10ee0596fb498d1f27c6704eb2a0d3ec4f5e88
yjj <wlswn0406@knou.ac.kr>
2021-04-26 10:10:49 +0900

    init

---
+++ src/@utils/storageUtil.js
@@ -0,0 +1,28 @@
+const localStorage = window.localStorage;
+const sessionStorage = window.sessionStorage;
+
+
+
+export const Cookie = {
+
+    get: {},
+    set: {},
+    del: {},
+
+};
+
+export const Local = {
+
+    get: {},
+    set: {},
+    del: {},
+
+};
+
+export const Session = {
+
+    get: {},
+    set: {},
+    del: {},
+
+};
